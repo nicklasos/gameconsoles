9d63d295769f489779be7df5d59e5010ad7d8884
Nicklasos <nicklasos@gmail.com>
2019-04-25 18:28:54 +0300

    Remove main page test conde

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,22 +2,8 @@
 
 @section('content')
 
-    <h1>All information about upcoming game releases</h1>
+    <h1>All information about video game consoles</h1>
 
     <h2>Opening soon</h2>
-
-    <p>
-        @include('img', [
-            'src' => '/img/bowser-1.jpg',
-            'alt' => 'bowser',
-            'w' => 538,
-            'h' => 538,
-        ])
-    </p>
-
-    <p>Video games</p>
-    <p>Kojima is genius!</p>
-
-    <p>{{ mt_rand() }}</p>
 
 @endsection
