d22bd9fdee4e18f17018ab7a34cdeda99dc2a9e5
Nicklasos <nicklasos@gmail.com>
2019-04-25 17:46:31 +0300

    Add base db structure

--- database/seeds/AdminTablesSeeder.php
+++ database/seeds/AdminTablesSeeder.php
@@ -29,90 +29,101 @@ class AdminTablesSeeder extends Seeder
                 [
                     "id" => 2,
                     "parent_id" => 0,
-                    "order" => 2,
+                    "order" => 5,
                     "title" => "Admin",
                     "icon" => "fa-tasks",
-                    "uri" => "",
-                    "permission" => NULL,
+                    "uri" => "consoles",
+                    "permission" => "*",
                     "created_at" => NULL,
-                    "updated_at" => NULL
+                    "updated_at" => "2019-04-25 13:02:00"
                 ],
                 [
                     "id" => 3,
                     "parent_id" => 2,
-                    "order" => 3,
+                    "order" => 6,
                     "title" => "Users",
                     "icon" => "fa-users",
                     "uri" => "auth/users",
                     "permission" => NULL,
                     "created_at" => NULL,
-                    "updated_at" => NULL
+                    "updated_at" => "2019-04-25 13:02:00"
                 ],
                 [
                     "id" => 4,
                     "parent_id" => 2,
-                    "order" => 4,
+                    "order" => 7,
                     "title" => "Roles",
                     "icon" => "fa-user",
                     "uri" => "auth/roles",
                     "permission" => NULL,
                     "created_at" => NULL,
-                    "updated_at" => NULL
+                    "updated_at" => "2019-04-25 13:02:00"
                 ],
                 [
                     "id" => 5,
                     "parent_id" => 2,
-                    "order" => 5,
+                    "order" => 8,
                     "title" => "Permission",
                     "icon" => "fa-ban",
                     "uri" => "auth/permissions",
                     "permission" => NULL,
                     "created_at" => NULL,
-                    "updated_at" => NULL
+                    "updated_at" => "2019-04-25 13:02:00"
                 ],
                 [
                     "id" => 6,
                     "parent_id" => 2,
-                    "order" => 6,
+                    "order" => 9,
                     "title" => "Menu",
                     "icon" => "fa-bars",
                     "uri" => "auth/menu",
                     "permission" => NULL,
                     "created_at" => NULL,
-                    "updated_at" => NULL
+                    "updated_at" => "2019-04-25 13:02:00"
                 ],
                 [
                     "id" => 7,
                     "parent_id" => 2,
-                    "order" => 7,
+                    "order" => 10,
                     "title" => "Operation log",
                     "icon" => "fa-history",
                     "uri" => "auth/logs",
                     "permission" => NULL,
                     "created_at" => NULL,
-                    "updated_at" => NULL
+                    "updated_at" => "2019-04-25 13:02:00"
                 ],
                 [
                     "id" => 8,
                     "parent_id" => 0,
-                    "order" => 0,
+                    "order" => 2,
                     "title" => "Companies",
                     "icon" => "fa-building",
                     "uri" => "companies",
                     "permission" => "*",
                     "created_at" => "2019-04-25 10:27:03",
-                    "updated_at" => "2019-04-25 10:27:03"
+                    "updated_at" => "2019-04-25 13:02:00"
                 ],
                 [
                     "id" => 9,
                     "parent_id" => 0,
-                    "order" => 0,
+                    "order" => 3,
                     "title" => "Consoles",
                     "icon" => "fa-gamepad",
                     "uri" => "consoles",
                     "permission" => "*",
                     "created_at" => "2019-04-25 10:27:46",
-                    "updated_at" => "2019-04-25 10:27:46"
+                    "updated_at" => "2019-04-25 13:02:00"
+                ],
+                [
+                    "id" => 10,
+                    "parent_id" => 0,
+                    "order" => 4,
+                    "title" => "Games",
+                    "icon" => "fa-soccer-ball-o",
+                    "uri" => "games",
+                    "permission" => "*",
+                    "created_at" => "2019-04-25 12:09:56",
+                    "updated_at" => "2019-04-25 13:02:00"
                 ]
             ]
         );
