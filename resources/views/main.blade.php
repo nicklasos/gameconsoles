147405ad1aace9595d918e3c2c824d8998af5832
Nicklasos <nicklasos@gmail.com>
2020-03-10 19:03:18 +0200

    *

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -17,50 +17,55 @@
 
     <hr class="mt-8">
 
-    <div class="mt-8 mb-10">
+    <div class="mt-8">
         @foreach ($consoles as $console)
-            <div class="flex mt-10">
-                <div class="max-w-xs self-start rounded-lg overflow-hidden shadow-lg">
-                    @include('img', [
-                        'src' => $console->getFirstMediaUrl('logo'),
-                        'alt' => $console->name,
-                        'class' => 'w-full object-cover'
-                    ])
-                    <div class="bg-white border-t px-3 py-3">
-                        <div class="flex justify-between justify-center">
-                            <div class="text-gray-600 text-xs tracking-wide font-semibold uppercase">{{ $console->company->name }}</div>
-                            <div class="font-light text-xs">{{ optional($console->released_at)->format('Y') }}</div>
+            <div class="mb-16">
+                <div class="pl-1 pb-2 text-gray-600 text-sm tracking-wide font-semibold uppercase">{{ $console->company->name }}</div>
+                <div class="flex">
+                    <div class="max-w-md self-start rounded-lg overflow-hidden shadow-lg">
+                        <div>
+                            @include('img', [
+                                'src' => $console->getFirstMediaUrl('logo'),
+                                'alt' => $console->name,
+                                'class' => 'w-full object-cover'
+                            ])
                         </div>
-                        <div class="font-semibold text-lg my-2">{{ $console->name }}</div>
-                    </div>
-                </div>
-                <div class="ml-10">
-                    <div class="flex">
-                        @foreach($console->children as $children)
-                            <div class="mr-4">
-                                <div class="pl-1 text-sm text-gray-700">{{ $children->name }}</div>
-                                @include('img', [
-                                    'src' => $children->getFirstMediaUrl('logo', 'thumb'),
-                                    'alt' => $children->name,
-                                    'class' => 'rounded-md mt-2 shadow-md'
-                                ])
-                                <div class="pl-1 text-xs text-gray-500 mt-1">{{ optional($children->released_at)->format('Y') }}</div>
+                        <div class="bg-white border-t px-3 py-3">
+                            <div class="flex justify-between justify-center content-center">
+                                <div class="font-semibold text-lg">{{ $console->name }}</div>
+                                <div class="font-light text-xs pt-1">{{ optional($console->released_at)->format('Y') }}</div>
                             </div>
-                        @endforeach
+                        </div>
                     </div>
 
-                    <div>
-                        @foreach ($console->games as $game)
-                            <div>
-                                <div class="pl-1 text-sm text-gray-700">{{ $game->name }}</div>
-                                @include('img', [
-                                    'src' => $game->getFirstMediaUrl('logo', 'thumb'),
-                                    'alt' => $game->name,
-                                    'class' => 'rounded-md mt-2 shadow-md'
-                                ])
-                                <div class="pl-1 text-xs text-gray-500 mt-1">{{ optional($game->released_at)->format('Y') }}</div>
-                            </div>
-                        @endforeach
+                    <div class="ml-10">
+                        <div class="flex">
+                            @foreach($console->children as $children)
+                                <div class="mr-4">
+                                    <div class="pl-1 text-sm text-gray-700 truncate w-16">{{ $children->name }}</div>
+                                    @include('img', [
+                                        'src' => $children->getFirstMediaUrl('logo', 'thumb'),
+                                        'alt' => $children->name,
+                                        'class' => 'rounded-md mt-2 shadow-md'
+                                    ])
+                                    <div class="pl-1 text-xs text-gray-500 mt-1">{{ optional($children->released_at)->format('Y') }}</div>
+                                </div>
+                            @endforeach
+                        </div>
+
+                        <div class="mt-4">
+                            @foreach ($console->games as $game)
+                                <div>
+                                    <div class="pl-1 text-sm text-gray-700">{{ $game->name }}</div>
+                                    @include('img', [
+                                        'src' => $game->getFirstMediaUrl('logo', 'thumb'),
+                                        'alt' => $game->name,
+                                        'class' => 'rounded-md mt-2 shadow-md h-24'
+                                    ])
+                                    <div class="pl-1 text-xs text-gray-500 mt-1">{{ optional($game->released_at)->format('Y') }}</div>
+                                </div>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
